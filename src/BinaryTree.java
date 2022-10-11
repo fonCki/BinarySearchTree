a785444af118b6ae020ac83fc2ee1d95ef72bc83
fonCki <alfonsoridao@gmail.com>
2022-10-11 16:36:29 +0200

    added assignment

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,2 +1,53 @@
-package PACKAGE_NAME;public class BinaryTree {
+import java.util.ArrayList;
+
+public class BinaryTree<T> {
+    private BinaryTreeNode<T> root;
+    private int size;
+
+    public BinaryTree() {
+        root = null;
+        size = 0;
+    }
+
+    public BinaryTreeNode<T> getRoot() {
+        return root;
+    }
+
+    public void setRoot(BinaryTreeNode<T> node) {
+        root = node;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean contains(T element) {
+        return contains(root, element);
+    }
+
+    private boolean contains(BinaryTreeNode<T> node, T element) {
+
+        return (root == null) ? false : root.getElement().equals(element) ||
+                                        contains(root.getLeftChild(), element) ||
+                                        contains(root.getRightChild(), element);
+    }
+
+    public ArrayList<T> inOrder() {
+        ArrayList<T> list = new ArrayList<T>();
+        inOrder(root, list);
+        return list;
+    }
+
+    private void inOrder(BinaryTreeNode<T> node, ArrayList<T> list) {
+        if (node != null) {
+            inOrder(node.getLeftChild(), list);
+            list.add(node.getElement());
+            inOrder(node.getRightChild(), list);
+        }
+    }
+
 }
--- src/BinaryTreeNode.java
+++ src/BinaryTreeNode.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class BinaryTreeNode {
+public class BinaryTreeNode<T> {
+    private T element;
+    private BinaryTreeNode<T> leftChild;
+    private BinaryTreeNode<T> rightChild;
+
+    public BinaryTreeNode(T element) {
+        this.element = element;
+    }
+
+    public T getElement() {
+        return element;
+    }
+
+    public void setElement(T element) {
+        this.element = element;
+    }
+
+    public void addLeftChild(BinaryTreeNode node) {
+        leftChild = node;
+    }
+
+    public void addRightChild(BinaryTreeNode node) {
+        rightChild = node;
+    }
+
+    public BinaryTreeNode getLeftChild() {
+        return leftChild;
+    }
+
+    public BinaryTreeNode getRightChild() {
+        return rightChild;
+    }
+
 }
